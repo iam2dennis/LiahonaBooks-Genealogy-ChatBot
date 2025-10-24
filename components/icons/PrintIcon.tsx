077ca3f4667fc9cc605f4275b30d4e3bf0fcfb58
iam2dennis <iam2dennis@gmail.com>
2025-10-23 22:55:32 -0400

    Add files via upload

--- components/icons/PrintIcon.tsx
+++ components/icons/PrintIcon.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-const CopyIcon: React.FC = () => (
+const PrintIcon: React.FC = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     className="h-5 w-5"
@@ -13,9 +13,9 @@ const CopyIcon: React.FC = () => (
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+      d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
     />
   </svg>
 );
 
-export default CopyIcon;
+export default PrintIcon;
