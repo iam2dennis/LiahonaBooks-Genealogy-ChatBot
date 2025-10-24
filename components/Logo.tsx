a3f8f105509aa6aee2bfb01f9e1d1518e95abb72
iam2dennis <iam2dennis@gmail.com>
2025-10-24 18:11:46 -0400

    Update Logo component to return null
    Fix fatal error by returning null in unused Logo component.

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,1 @@
-import React from 'react';
 
-const Logo: React.FC = () => {
-  // This component is currently unused but was causing a fatal error by not returning a value.
-  // Returning null makes it a valid, non-rendering component and fixes the application crash.
-  return null;
-};
-
-export default Logo;
