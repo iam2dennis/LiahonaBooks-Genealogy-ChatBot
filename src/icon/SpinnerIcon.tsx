4ae5365be4c3abdfe78891ef3103ed7555d76bd4
iam2dennis <iam2dennis@gmail.com>
2025-10-25 17:26:54 -0400

    Add files via upload

--- src/icon/SpinnerIcon.tsx
+++ src/icon/SpinnerIcon.tsx
@@ -1,21 +1,27 @@
 
 import React from 'react';
 
-const PrintIcon: React.FC = () => (
+const SpinnerIcon: React.FC = () => (
   <svg
+    className="animate-spin h-5 w-5 text-white"
     xmlns="http://www.w3.org/2000/svg"
-    className="h-5 w-5"
     fill="none"
     viewBox="0 0 24 24"
-    stroke="currentColor"
-    strokeWidth={2}
   >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="4"
+    ></circle>
     <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
-    />
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    ></path>
   </svg>
 );
 
-export default PrintIcon;
+export default SpinnerIcon;
